fa6f50733dac2c8f20d45318f1d9b829e4e2858c
mausch <mauricioscheffer@gmail.com>
2011-08-09 19:39:33 -0300

    more references

---
+++ FigContactManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.fs" Inherits="FigContactManager.MvcApplication, FigContactManager" Language="F#" %>
---
+++ FigContactManager/Global.asax.fs
@@ -0,0 +1,10 @@
+﻿namespace FigContactManager
+
+open System
+open System.Web
+open System.Web.Mvc
+
+type MvcApplication() =
+    inherit HttpApplication()
+    member this.Application_Start() = 
+        ()
