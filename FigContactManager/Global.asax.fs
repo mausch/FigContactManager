6d1c1d0c67b7ba0d3eb8e32f837890f63b2fef62
mausch <mauricioscheffer@gmail.com>
2011-08-11 20:45:26 -0300

    started with initial database

--- FigContactManager/Global.asax.fs
+++ FigContactManager/Global.asax.fs
@@ -3,8 +3,24 @@
 open System
 open System.Web
 open System.Web.Mvc
+open FigContactManager.Data
 
 type MvcApplication() =
     inherit HttpApplication()
+
+    let tx = Tx.TransactionBuilder()
+    let InitializeDatabase() =
+        let cs = System.Configuration.ConfigurationManager.ConnectionStrings.["sqlite"].ConnectionString
+        use conn = createConnection cs
+        createSchema conn [typeof<Contact>; typeof<Group>; typeof<ContactGroup>]
+        let t =
+            tx {
+                let! john = insertContact { Contact.Id = 0L; Name = ""; Phone = ""; Email = "" }
+                return 0
+            }
+        t (Sql.withConnection conn) |> Tx.getOrFail ignore
+        ()
+
     member this.Application_Start() = 
+        InitializeDatabase()
         ()
