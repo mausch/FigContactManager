68361269545849cf4b3d38217d547ffe7650ae0f
mausch <mauricioscheffer@gmail.com>
2011-08-11 15:37:46 -0300

    started defining data access

---
+++ FigContactManager.Tests/Data.fs
@@ -0,0 +1,35 @@
+﻿module FigContactManager.Tests.Data
+
+open System
+open System.Collections.Generic
+open System.Data
+open MbUnit.Framework
+open Microsoft.FSharp.Reflection
+open FigContactManager.Data
+
+let createConnection() =
+    let conn = new System.Data.SQLite.SQLiteConnection("Data Source=:memory:;Version=3;New=True")
+    conn.Open()
+    conn :> IDbConnection
+
+[<Test>]
+let ``create contact``() =
+    let tx = Tx.TransactionBuilder()
+    use conn = createConnection()
+    let mgr = Sql.withConnection conn
+    createSchema conn [typeof<Contact>]
+    let insert =
+        tx {
+            let! i = insertContact { Id = 0L; Name = "John"; Phone = "555-1234"; Email = "john@example.com" }
+            let! j = insertContact { Id = 0L; Name = "George"; Phone = "555-4447"; Email = "george@example.com" }
+            printfn "%A" i
+            printfn "%A" j
+            return i,j
+        }
+    match insert mgr with
+    | Tx.Commit (c1,c2) ->
+        Assert.AreEqual(1L, c1.Id)
+        Assert.AreEqual(2L, c2.Id)
+    | Tx.Failed e -> raise e
+    | Tx.Rollback _ -> failwith "rollback"
+    ()
---
+++ FigContactManager/Data.fs
@@ -0,0 +1,79 @@
+﻿namespace FigContactManager
+
+open System
+open System.Collections.Generic
+open System.Data
+open Microsoft.FSharp.Reflection
+
+module Data =
+    type Contact = {
+        Id: int64
+        Name: string
+        Phone: string
+        Email: string
+    }
+
+    type Group = {
+        Id: int64
+        Name: string
+    }
+
+    type ContactGroup = {
+        Group: int64
+        Contact: int64
+    }
+
+    let internal strEq (a: string) (b: string) = 
+        StringComparer.InvariantCultureIgnoreCase.Equals(a, b)
+
+    let createSchema conn types =
+        let exec a = Sql.execNonQuery (Sql.withConnection conn) a [] |> ignore
+        let sqlType t =
+            match t with
+            | x when x = typeof<int> -> "int"
+            | x when x = typeof<string> -> "varchar"
+            | x when x = typeof<bool> -> "int"
+            | x when x = typeof<DateTime> -> "datetime"
+            | x -> failwithf "Don't know how to express type %A in database" x
+        let keywords = HashSet<_>(["order"], StringComparer.InvariantCultureIgnoreCase)
+        let escape s =
+            if keywords.Contains s
+                then sprintf "\"%s\"" s // sqlite-specific quote
+                else s
+        let createTable (escape: string -> string) (sqlType: Type -> string) (t: Type) =
+            let fields = FSharpType.GetRecordFields t |> Seq.filter (fun p -> strEq p.Name "id" |> not)
+            let table = escape t.Name
+            let drop = sprintf "drop table if exists %s" table
+            let fields = 
+                let fieldType (t: Type) =
+                    let nullable = t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>>
+                    let sqlType = 
+                        let t = 
+                            if nullable
+                                then t.GetGenericArguments().[0]
+                                else t
+                        sqlType t
+                    let nullable = if nullable then "" else "not"
+                    sprintf "%s %s null" sqlType nullable
+                fields |> Seq.map (fun f -> sprintf "%s %s" (escape f.Name) (fieldType f.PropertyType))
+            let fields = String.Join(",", Seq.toArray fields)
+            let create = sprintf "create table %s (id integer primary key autoincrement, %s)" table fields
+            //printfn "%s" create
+            exec drop
+            exec create
+            ()
+        types |> Seq.iter (createTable escape sqlType)
+
+
+    let internal P = Sql.Parameter.make
+
+    let internal (>>=) f x = Tx.bind x f
+
+    let internal selectLastId = "select last_insert_rowid();"
+
+    let insertContact (c: Contact) =
+        let sql = "insert into contact values (null, @n, @p, @e);" + selectLastId
+        Tx.execScalar sql
+            [P("@n", c.Name); P("@p", c.Phone); P("@e", c.Email)] 
+        |> Tx.map Option.get
+        |> Tx.map (fun newId -> { c with Id = newId })
