3a1ab652ecc60392826573720e9b6b9b985accfd
mausch <mauricioscheffer@gmail.com>
2011-08-26 15:37:44 -0300

    testing contact formlet

--- FigContactManager.Tests/Web.fs
+++ FigContactManager.Tests/Web.fs
@@ -1,5 +1,6 @@
 ﻿module FigContactManager.Web.Tests
 
+open Formlets
 open MbUnit.Framework
 open FigContactManager
 open FigContactManager.Data
@@ -13,4 +14,16 @@ let ``show all groups`` () =
     let cmgr = App.InitializeDatabase connectionString
     let html = showAllGroups cmgr |> Renderer.RenderToString
     printfn "%s" html
+    ()
+
+[<Test>]
+let ``contact formlet with empty phone and email gives error``() =
+    let env = EnvDict.fromStrings [losSerializer.Serialize 1L; "John"; ""; ""]
+    printfn "%A" env
+    let r = run (contactFormlet Contact.Dummy) env
+    match r with
+    | Success c -> failwithf "formlet should not have succeeded: %A" c
+    | Failure (_,errors) -> 
+        Assert.AreEqual(1, errors.Length)
+        Assert.AreEqual("Enter either a phone or an email", errors.[0])
     ()
