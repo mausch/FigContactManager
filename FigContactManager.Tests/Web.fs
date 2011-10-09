0ff492bc65fd0c5e8940eacbe71cc7b121c06676
mausch <mauricioscheffer@gmail.com>
2011-10-08 23:40:15 -0300

    testing deleteContact

--- FigContactManager.Tests/Web.fs
+++ FigContactManager.Tests/Web.fs
@@ -1,14 +1,21 @@
 ﻿module FigContactManager.Web.Tests
 
+open FSharpx
 open Formlets
 open MbUnit.Framework
 open FigContactManager
 open FigContactManager.Data
 open FigContactManager.Model
 open FigContactManager.Data.Tests
+open FigContactManager.Web.Actions
 open FigContactManager.Web.Views
 open WingBeats
 open WingBeats.Xml
+open Figment
+open Figment.Testing
+open System.Web
+open System.Web.Mvc
+open System.Web.Routing
 
 [<Test>]
 let ``contact formlet with empty phone and email gives error``() =
@@ -19,3 +26,24 @@ let ``contact formlet with empty phone and email gives error``() =
     | Failure (_,errors) -> 
         Assert.AreEqual(1, errors.Length)
         Assert.AreEqual("Enter either a phone or an email", errors.[0])
+
+[<Test>]
+let ``delete contact with inexisting contact``() =
+    use conn = createConnection()
+    let mgr = Sql.withConnection conn
+    createSchema mgr [typeof<Contact>; typeof<ContactGroup>]
+    let serialized = losSerializer.Serialize (1L,1L)
+    let redirectTo = ref ""
+    let cookie : HttpCookie ref = ref null
+    let ctx = 
+        buildRequest "GET" ""
+        |> withForm (NameValueCollection.fromSeq ["f0",serialized])
+        |> withFiles []
+        |> withResponse
+            { new HttpResponseBase() with
+                override x.Redirect(url, _) = redirectTo := url 
+                override x.SetCookie c = cookie := c }
+        |> buildCtx
+    deleteContact mgr ctx
+    Assert.AreEqual("/contacts", !redirectTo)
+    Assert.Contains(base64decode (!cookie).Value, "deleted or modified")
